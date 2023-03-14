9f89cbdc0b2ec0b314ba1127384d2909150ad215
AlexandeChang <1264064757@qq.com>
2023-03-14 15:47:22 +0800

    Initial commit

---
+++ src/main/java/com/ebs/custom_table/CustomTableApplication.java
@@ -0,0 +1,13 @@
+package com.ebs.custom_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebs/custom_table/CustomTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebs.custom_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
